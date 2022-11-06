63d36570021049cb46a837e172fcd67da9ced4a0
ALEJANDRO SANCHEZ <aleenetflix95@gmail.com>
2022-11-06 04:22:21 -0300

    MT-F2-06

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,9 +3,6 @@ import AutoToTop from '../components/AutoToTop'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
-
-
-
 export default function layout(props) {
     return (
         <>
