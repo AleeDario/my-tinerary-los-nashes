4e2427999961aefff7e3262d2cc31710835d1ffe
ALEJANDRO SANCHEZ <aleenetflix95@gmail.com>
2022-11-07 06:20:20 -0300

    FIX-MT-F2-10

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -4,11 +4,15 @@ import { Link as NavLink } from 'react-router-dom'
 export default function HotelCard(props) {
     let { hotel} = props
 
+    function numeroRandom(numero) {
+        return Math.floor(Math.random() * numero)
+    }
+
     return (
         <div className="card-container bg-palette1 flex column justify-center" >
             <div className="img-card-container">
                 <img className="img-card"
-                    src={hotel.photo[0]}
+                    src={hotel.photo[numeroRandom(hotel.photo.length - 1)]}
                     alt={hotel.name} />
             </div>
             <div className="text-card">
