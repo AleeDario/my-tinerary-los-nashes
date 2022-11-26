563a5b046cb56b894c03478284831fffe10d7fad
Meli Uribe <lissauribe21@gmail.com>
2022-11-25 20:01:59 -0500

    MT-02-46

--- src/App.js
+++ src/App.js
@@ -14,29 +14,44 @@ import MyCities from "./pages/MyCities";
 import MyHotels from "./pages/MyHotels"
 import MyItineraries from "./pages/MyItineraries";
 import MyShows from "./pages/MyShows";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import userActions from "./redux/actions/userActions";
 
 function App() {
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/cities" element={<Cities />} />
-        <Route path="/details/:id" element={<DetailsCity/>} />
-        <Route path="/hotels" element={<Hotels />} />
-        <Route path="/detailsH/:id" element={<DetailsHotels />} />
-        <Route path="/newcity" element={<NewCity />} />
-        <Route path="/newhotel" element={<NewHotel />} />
-        <Route path="/mycities" element={<MyCities />} />
-        <Route path="/myhotels" element={<MyHotels />} />
-        <Route path="/myitineraries" element={<MyItineraries />} />
-        <Route path="/myshows" element={<MyShows />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </Layout>
-  );
+
+    const dispatch = useDispatch()
+    const { reLogin } = userActions
+    const token = JSON.parse(localStorage.getItem("token"))
+
+    useEffect(() => {
+        if (token) {
+            dispatch(reLogin(token.token.user))
+        }
+        // eslint-disable-next-line
+    }, [])
+
+    return (
+        <Layout>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/signUp" element={<SignUp />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/cities" element={<Cities />} />
+                <Route path="/details/:id" element={<DetailsCity />} />
+                <Route path="/hotels" element={<Hotels />} />
+                <Route path="/detailsH/:id" element={<DetailsHotels />} />
+                <Route path="/newcity" element={<NewCity />} />
+                <Route path="/newhotel" element={<NewHotel />} />
+                <Route path="/mycities" element={<MyCities />} />
+                <Route path="/myhotels" element={<MyHotels />} />
+                <Route path="/myitineraries" element={<MyItineraries />} />
+                <Route path="/myshows" element={<MyShows />} />
+                <Route path="/*" element={<NotFound />} />
+            </Routes>
+        </Layout>
+    );
 }
 
 export default App;
